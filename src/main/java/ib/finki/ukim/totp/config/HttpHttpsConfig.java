c760a3c9df7b0ba2eb807f749c58ddb7f9126996
GjorgiNechovski <gjorginechovski@gmail.com>
2024-06-19 15:16:55 +0200

    temporary removed https support

--- src/main/java/ib/finki/ukim/totp/config/HttpHttpsConfig.java
+++ src/main/java/ib/finki/ukim/totp/config/HttpHttpsConfig.java
@@ -15,37 +15,37 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HttpHttpsConfig {
 
-    /**
-     * Configures the servlet container to handle HTTP to HTTPS redirection.
-     */
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                var securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                var collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(getHttpConnector());
-        return tomcat;
-    }
-
-    /**
-     * Configures an HTTP connector.
-     *
-     * @return The configured HTTP connector.
-     */
-    private Connector getHttpConnector() {
-        var connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setScheme("http");
-        connector.setPort(8080);
-        connector.setSecure(false);
-        connector.setRedirectPort(443);
-        return connector;
-    }
+//    /**
+//     * Configures the servlet container to handle HTTP to HTTPS redirection.
+//     */
+//    @Bean
+//    public ServletWebServerFactory servletContainer() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                var securityConstraint = new SecurityConstraint();
+//                securityConstraint.setUserConstraint("CONFIDENTIAL");
+//                var collection = new SecurityCollection();
+//                collection.addPattern("/*");
+//                securityConstraint.addCollection(collection);
+//                context.addConstraint(securityConstraint);
+//            }
+//        };
+//        tomcat.addAdditionalTomcatConnectors(getHttpConnector());
+//        return tomcat;
+//    }
+//
+//    /**
+//     * Configures an HTTP connector.
+//     *
+//     * @return The configured HTTP connector.
+//     */
+//    private Connector getHttpConnector() {
+//        var connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+//        connector.setScheme("http");
+//        connector.setPort(8080);
+//        connector.setSecure(false);
+//        connector.setRedirectPort(443);
+//        return connector;
+//    }
 }
